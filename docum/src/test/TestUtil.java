19acd7d869fb2a72a92eddd6859b64e43ff481b7
alekseyzet@gmail.com <alekseyzet@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-05-02 19:45:17 +0000

    Утилитарный класс для тестирования

---
+++ docum/src/test/TestUtil.java
@@ -0,0 +1,46 @@
+package test;
+
+import java.util.Date;
+import java.util.Random;
+
+public class TestUtil {
+
+	private static Random random;
+	private static String initialString =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	private static final Long ONE_YEAR_MILLIS = 31536000000L;
+	private static final int epoch = 1970;
+
+	static {
+		random = new Random();
+	}
+
+	public static Long getRandomLong() {
+		return Math.abs(random.nextLong());
+	}
+
+	public static Integer getRandomInteger() {
+		return Math.abs(random.nextInt());
+	}	
+
+	public static Integer getRandomInteger(int maximum) {
+		return Math.abs(random.nextInt(maximum));
+	}
+
+	public static Date getRandomDate(int maxYear) {
+		Long date = random.nextInt(maxYear-epoch) * ONE_YEAR_MILLIS;
+		return new Date(date);
+	}
+
+	public static String getRandomString(int length) {
+		String result = "";
+		int initialSize = initialString.length();
+		char[] initialChars = initialString.toCharArray() ;
+
+		for (int i = 0; i < length; i++) {
+			result += initialChars[getRandomInteger(initialSize - 1)];
+		}
+
+		return result;
+	}
+}
