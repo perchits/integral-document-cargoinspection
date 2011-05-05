af7b1b1027ec7744d20d97bcf615711f440e7cc6
perchits@gmail.com <perchits@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-05-05 18:25:05 +0000

    Подружил String и JPA Сделал возможность запуска тестов в контексте транзакций Spring

--- docum/src/com/docum/dao/impl/BaseDaoImpl.java
+++ docum/src/com/docum/dao/impl/BaseDaoImpl.java
@@ -1,33 +1,32 @@
 package com.docum.dao.impl;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
 
 import com.docum.dao.BaseDao;
 import com.docum.persistence.DocumEntity;
 
-public class BaseDaoImpl extends HibernateDaoSupport implements BaseDao {
+public class BaseDaoImpl implements BaseDao {
 	
+	@PersistenceContext(name="docum")
+	protected EntityManager entityManager;
+
+	protected BaseDaoImpl() {
+	}
+	
+	@Transactional
 	public <T extends DocumEntity> Long saveObject(T object) {
 		Long id = null;
-
-    	Session session = getSession();
-        Transaction tx = session.beginTransaction();
-    	session.saveOrUpdate(object);
-        tx.commit();
-        session.close();
-
+		entityManager.persist(object);
 		id = object.getId();
 		return id;
 	}
-	
+
+	@Transactional
 	public <T extends DocumEntity> void deleteObject(T object) {
-    	Session session = getSession();
-        Transaction tx = session.beginTransaction();
-    	session.delete(object);
-        tx.commit();
-        session.close();
+		entityManager.remove(object);
 	}
 
 }
