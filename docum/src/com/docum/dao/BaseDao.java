48cf1f8d1dbae4ff39453302bbbf357a3092f11c
perchits@gmail.com <perchits@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-05-13 11:41:11 +0000

    Разборки с транзакциями

--- docum/src/com/docum/dao/BaseDao.java
+++ docum/src/com/docum/dao/BaseDao.java
@@ -7,7 +7,7 @@ import com.docum.persistence.IdentifiedEntity;
 
 public interface BaseDao extends Serializable {
 	
-	public <T extends IdentifiedEntity> Long saveObject(T object);
+	public <T extends IdentifiedEntity> T saveObject(T object);
 	
 	public <T extends IdentifiedEntity> void deleteObject(T object);
 	
--- docum/src/com/docum/dao/impl/SupplierDaoImpl.java
+++ docum/src/com/docum/dao/impl/SupplierDaoImpl.java
@@ -1,11 +1,11 @@
 package com.docum.dao.impl;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import com.docum.dao.SupplierDao;
 import com.docum.persistence.common.Supplier;
 
-@Service("supplierDao")
+@Repository("supplierDao")
 public class SupplierDaoImpl extends BaseDaoImpl implements SupplierDao {
 	private static final long serialVersionUID = 2696884917423567369L;
 
