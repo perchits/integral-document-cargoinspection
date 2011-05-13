b09d21973bd1ca9ae674ebbd8a2aea534dfa0a10
perchits@gmail.com <perchits@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-05-13 12:49:44 +0000

    Реализация дашборда

---
+++ trunk/docum/src/com/docum/util/AlgoUtil.java
@@ -0,0 +1,266 @@
+/**
+ * 
+ */
+package com.docum.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author pepper
+ * 
+ */
+public class AlgoUtil
+{
+    public static abstract class CopyFunctor<T> implements TransformFunctor<T, T>
+    {
+        @Override
+        public T transform(T from)
+        {
+            return copy(from);
+        }
+        public abstract T copy(T from);
+    }
+    public static <T> Collection<T> copy(Collection<T> dst,
+            Collection<T> src, CopyFunctor<T> functor)
+    {
+        return transform(dst, src, functor);
+    }
+
+    public static interface TransformFunctor<ToT, FromT>
+    {
+        public ToT transform(FromT from);
+    }
+
+    public static <ToT, FromT> Collection<ToT> transform(Collection<ToT> dst,
+            Collection<FromT> src, TransformFunctor<ToT, FromT> functor)
+    {
+        dst.clear();
+        if(dst instanceof ArrayList)
+            ((ArrayList<ToT>) dst).ensureCapacity(src.size());
+        for(FromT from : src)
+            dst.add(functor.transform(from));
+        return dst;
+    }
+
+    public static <ToKeyT, ToValueT, FromT> Map<ToKeyT, ToValueT> transform(
+            Map<ToKeyT, ToValueT> dst, Collection<FromT> src,
+            TransformFunctor<Map.Entry<ToKeyT, ToValueT>, FromT> functor)
+    {
+        dst.clear();
+        for(FromT from : src)
+        {
+            Map.Entry<ToKeyT, ToValueT> entry = functor.transform(from);
+            dst.put(entry.getKey(), entry.getValue());
+        }
+        return dst;
+    }
+
+    public static <ToT, FromKeyT, FromValueT> Collection<ToT> transform(Collection<ToT> dst,
+            Map<FromKeyT, FromValueT> src,
+            TransformFunctor<ToT, Map.Entry<FromKeyT, FromValueT>> functor)
+    {
+        dst.clear();
+        if(dst instanceof ArrayList)
+            ((ArrayList<ToT>) dst).ensureCapacity(src.size());
+        for(Map.Entry<FromKeyT, FromValueT> from : src.entrySet())
+            dst.add(functor.transform(from));
+        return dst;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <ToT, FromT> ToT[] transform(FromT[] src, TransformFunctor<ToT, FromT> functor)
+    {
+      ArrayList<ToT> dst = new ArrayList<ToT>(src.length);
+      return (ToT[])transform(dst, Arrays.asList(src), functor).toArray();
+    }
+
+    public static class MapEntry<KeyT, ValueT> implements Map.Entry<KeyT, ValueT>
+    {
+        public MapEntry(KeyT key, ValueT value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        public KeyT getKey()
+        {
+            return key;
+        }
+
+        public ValueT getValue()
+        {
+            return value;
+        }
+
+        public ValueT setValue(ValueT value)
+        {
+            ValueT oldValue = this.value;
+            this.value = value;
+            return oldValue;
+        }
+
+        private KeyT key;
+        private ValueT value;
+    }
+
+    public static interface ApplyFunctor<T>
+    {
+        public void apply(T o);
+    }
+
+    public static <T> void forEach(Collection<T> c, ApplyFunctor<T> f)
+    {
+        for(T o : c)
+        {
+            f.apply(o);
+        }
+    }
+
+    public static <T> void forEach(T[] c, ApplyFunctor<T> f)
+    {
+        for(T o : c)
+        {
+            f.apply(o);
+        }
+    }
+
+    public static <T> int indexOf(Collection<T> c, T o)
+    {
+        if(o == null)
+        {
+            return -1;
+        }
+        else if(c instanceof List)
+        {
+            return ((List<T>) c).indexOf(o);
+        }
+        else
+        {
+            int i = 0;
+            for(T obj : c)
+            {
+                if(o.equals(obj))
+                {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
+        }
+    }
+
+    public static <T> int indexOf(T[] c, T o)
+    {
+        if(o == null)
+        {
+            return -1;
+        }
+        else
+        {
+            int i = 0;
+            for(T obj : c)
+            {
+                if(o.equals(obj))
+                {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
+        }
+    }
+
+    public static interface FindPredicate<T>
+    {
+        public boolean isIt(T o);
+    }
+
+    public static <T> T find(Collection<T> c, FindPredicate<T> f)
+    {
+        for(T o : c)
+        {
+            if(f.isIt(o))
+            {
+                return o;
+            }
+        }
+        return null;
+    }
+
+    public static <T> T find(T[] c, FindPredicate<T> f)
+    {
+        for(T o : c)
+        {
+            if(f.isIt(o))
+            {
+                return o;
+            }
+        }
+        return null;
+    }
+
+    public static <T> Collection<T> collect(Collection<T> dst, Collection<T> src, FindPredicate<T> f)
+    {
+        if(dst instanceof ArrayList)
+            ((ArrayList<T>) dst).ensureCapacity(src.size());
+        for(T o : src)
+        {
+            if(f.isIt(o))
+            {
+                dst.add(o);
+            }
+        }
+        return dst;
+    }
+
+    public static <T> Collection<T> collect(Collection<T> dst, T[] src, FindPredicate<T> f)
+    {
+        if(dst instanceof ArrayList)
+            ((ArrayList<T>) dst).ensureCapacity(src.length);
+        for(T o : src)
+        {
+            if(f.isIt(o))
+            {
+                dst.add(o);
+            }
+        }
+        return dst;
+    }
+    
+    public static interface AggregateFunctor<AggregatorT, AggregateeT, AggregationMarkerT>
+    {
+        public AggregationMarkerT getAggregationMarker(AggregateeT from);
+        public AggregatorT createAggregator(AggregationMarkerT marker);
+        public void addAggregation(AggregatorT aggregator, AggregateeT aggregatee);
+    }
+
+    public static <AggregatorT, AggregateeT, AggregationMarkerT>
+    Collection<AggregatorT> aggregate(Collection<AggregatorT> dst,
+            Collection<AggregateeT> src,
+            AggregateFunctor<AggregatorT, AggregateeT, AggregationMarkerT> functor)
+    {
+        dst.clear();
+        Set<AggregationMarkerT> markers = new HashSet<AggregationMarkerT>(); 
+        for(AggregateeT from : src)
+            markers.add(functor.getAggregationMarker(from));
+        if(dst instanceof ArrayList)
+            ((ArrayList<AggregatorT>) dst).ensureCapacity(markers.size());
+        for(AggregationMarkerT marker : markers)
+        {
+            AggregatorT aggregator = functor.createAggregator(marker);
+            for(AggregateeT aggregatee : src)
+            {
+                if(functor.getAggregationMarker(aggregatee).equals(marker))
+                    functor.addAggregation(aggregator, aggregatee);
+            }
+            dst.add(aggregator);
+        }
+        return dst;
+    }
+}
--- trunk/docum/src/com/docum/view/DashBoardView.java
+++ trunk/docum/src/com/docum/view/DashBoardView.java
@@ -5,14 +5,21 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
+import com.docum.domain.ContainerStateEnum;
+import com.docum.persistence.common.Container;
 import com.docum.persistence.common.Voyage;
 import com.docum.service.VoyageService;
+import com.docum.util.AlgoUtil;
 
 @ManagedBean(name = "dashboardView")
 @ViewScoped
@@ -25,19 +32,23 @@ public class DashBoardView implements Serializable {
 		this.voyageService = voyageService;
 	}
 	
-	private ArrayList<Voyage> finishedVoyages;
-	private ArrayList<Voyage> unfinishedVoyages;
+	private ArrayList<VoyagePresentation> finishedVoyages;
+	private ArrayList<VoyagePresentation> unfinishedVoyages;
 
-	public Collection<Voyage> getFinishedVoyages() {
+	public Collection<VoyagePresentation> getFinishedVoyages() {
 		if(finishedVoyages == null){
-			finishedVoyages = new ArrayList<Voyage>(voyageService.getFinishedVoyages());
+			Collection<Voyage> voyages = voyageService.getFinishedVoyages();
+			finishedVoyages = new ArrayList<VoyagePresentation>(voyages.size());
+			AlgoUtil.transform(finishedVoyages, voyages, new VoyageTransformer());
 		}
 		return finishedVoyages;
 	}
 
-	public Collection<Voyage> getUnfinishedVoyages() {
+	public Collection<VoyagePresentation> getUnfinishedVoyages() {
 		if(unfinishedVoyages == null){
-			unfinishedVoyages = new ArrayList<Voyage>(voyageService.getUnfinishedVoyages());
+			Collection<Voyage> voyages = voyageService.getFinishedVoyages();
+			unfinishedVoyages = new ArrayList<VoyagePresentation>(voyages.size());
+			AlgoUtil.transform(unfinishedVoyages, voyages, new VoyageTransformer());
 		}
 		return unfinishedVoyages;
 	}
@@ -67,12 +78,12 @@ public class DashBoardView implements Serializable {
 	public void sortByOrderNo() {
 
 		// ascending order
-		Collections.sort(finishedVoyages, new Comparator<Voyage>() {
+		Collections.sort(finishedVoyages, new Comparator<VoyagePresentation>() {
 
 			@Override
-			public int compare(Voyage o1, Voyage o2) {
+			public int compare(VoyagePresentation o1, VoyagePresentation o2) {
 
-				return o1.getVessel().getName().compareTo(o2.getVessel().getName());
+				return o1.getVessel().compareTo(o2.getVessel());
 
 			}
 
@@ -80,4 +91,71 @@ public class DashBoardView implements Serializable {
 
 	}
 
+	public static class VoyagePresentation implements Serializable {
+		private static final long serialVersionUID = -3545320838886096320L;
+		private Voyage voyage;
+		private Map<ContainerStateEnum, Integer> containerStateCounts =
+			new HashMap<ContainerStateEnum, Integer>();
+		
+		VoyagePresentation() {
+			containerStateCounts.put(ContainerStateEnum.BEFORE_CUSTOMS, 0);
+			containerStateCounts.put(ContainerStateEnum.AFTER_CUSTOMS, 0);
+			containerStateCounts.put(ContainerStateEnum.CHECKED, 0);
+			containerStateCounts.put(ContainerStateEnum.FINISHED, 0);
+		}
+		
+		public VoyagePresentation(Voyage voyage) {
+			this();
+			this.voyage = voyage;
+			for(Container container : voyage.getContainers()){
+				Integer count = containerStateCounts.get(container.getState());
+				count++;
+				containerStateCounts.put(container.getState(), count);
+			}
+		}
+		
+		public String getVessel() {
+			return voyage.getVessel().getName();
+		}
+		
+		public String getNumber() {
+			return voyage.getNumber();
+		}
+		
+		public Date getArrivalDate() {
+			return voyage.getArrivalDate();
+		}
+
+		public List<Container> getContainers() {
+			return voyage.getContainers();
+		}
+		
+		public Integer getTotalContainerCount(){
+			return voyage.getContainers().size();
+		}
+		
+		public Integer getBeforeCustomsContainerCount() {
+			return containerStateCounts.get(ContainerStateEnum.BEFORE_CUSTOMS);
+		}
+
+		public Integer getAfterCustomsContainerCount() {
+			return containerStateCounts.get(ContainerStateEnum.AFTER_CUSTOMS);
+		}
+
+		public Integer getCheckedContainerCount() {
+			return containerStateCounts.get(ContainerStateEnum.CHECKED);
+		}
+		
+		public Integer getFinishedContainerCount() {
+			return containerStateCounts.get(ContainerStateEnum.FINISHED);
+		}
+	}
+	
+	private static class VoyageTransformer
+		implements AlgoUtil.TransformFunctor<VoyagePresentation, Voyage> {
+		@Override
+		public VoyagePresentation transform(Voyage voyage) {
+			return new VoyagePresentation(voyage);
+		}
+	}
 }
