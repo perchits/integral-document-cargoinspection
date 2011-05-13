48d9b6754cbe4e8a90b29f4019b77da65f121d44
perchits@gmail.com <perchits@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-05-13 14:30:03 +0000

    Поправил заливку тестовых данных Добавил тест по рейсам Поправил конфигурацию

--- trunk/docum/src/test/dao/TestSupplierDao.java
+++ trunk/docum/src/test/dao/TestSupplierDao.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import test.TestUtil;
@@ -17,7 +16,6 @@ import com.docum.service.SupplierService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/docum-context.xml")
-@TransactionConfiguration(transactionManager="documTxManager")
 @Transactional
 public class TestSupplierDao {
 	@Autowired
