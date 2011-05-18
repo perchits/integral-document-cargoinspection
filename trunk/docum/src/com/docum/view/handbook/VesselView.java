eb305fd5d0c065da98119f6ffc2633e4e7366ae9
alekseyzet@gmail.com <alekseyzet@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-05-18 13:54:42 +0000

    1. Унификация CRUD панели в справочнике судов

--- trunk/docum/src/com/docum/view/handbook/VesselView.java
+++ trunk/docum/src/com/docum/view/handbook/VesselView.java
@@ -5,20 +5,24 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 
+import com.docum.persistence.IdentifiedEntity;
 import com.docum.persistence.common.Vessel;
 import com.docum.service.VesselService;
+import com.docum.view.handbook.dialog.BaseDialog;
 
-@ManagedBean(name = "vessel")
-@ViewScoped
-public class VesselView implements Serializable {
+@ManagedBean(name = "vesselBean")
+@SessionScoped
+public class VesselView extends BaseDialog implements Serializable {
 	private static final long serialVersionUID = -7018249724051865904L;
 
 	@ManagedProperty(value = "#{vesselService}")
 	private VesselService vesselService;
 
+	private static final String sing = "Судно";
 	private List<Vessel> vessels;
+	private Vessel vessel = new Vessel();
 	
 	public List<Vessel> getAllVessels() {
 		if(vessels == null) {
@@ -29,5 +33,28 @@ public class VesselView implements Serializable {
 
 	public void setVesselService(VesselService vesselService) {
 		this.vesselService = vesselService;
+	}
+
+	@Override
+	public String getSing() {
+		return sing;
+	}
+
+	@Override
+	public String getBase() {
+		return this.vessel.getName();
+	}
+
+	@Override
+	public IdentifiedEntity getBeanObject() {
+		return this.vessel;
+	}
+
+	public Vessel getVessel() {
+		return vessel;
+	}
+
+	public void setVessel(Vessel vessel) {
+		this.vessel = vessel;
 	}
 }
