ab9cd9120283f558cb7fec32bd4d33e43e2f139e
alekseyzet@gmail.com <alekseyzet@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-05-19 12:56:18 +0000

    Рефакторинг

--- trunk/docum/src/com/docum/view/handbook/SupplierView.java
+++ trunk/docum/src/com/docum/view/handbook/SupplierView.java
@@ -1,48 +1,22 @@
 package com.docum.view.handbook;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 import com.docum.persistence.IdentifiedEntity;
 import com.docum.persistence.common.Supplier;
-import com.docum.service.SupplierService;
 
 @ManagedBean(name = "supplierBean")
 @SessionScoped
 public class SupplierView extends BaseView implements Serializable {
 	private static final long serialVersionUID = -676095247499740650L;
+	
 	private static final String sign = "Поставщик";
-	@ManagedProperty(value = "#{supplierService}")
-	private SupplierService supplierService;
-
-	private List<Supplier> suppliers;
 	private Supplier supplier = new Supplier();
 
-	public List<Supplier> getSuppliers() {
-		if (suppliers == null) {
-			refreshSuppliers();
-		}
-		return suppliers;
-	}
-
-	public void deleteSupplier() {
-		supplierService.deleteSupplier(supplierService.getSupplier(supplier
-				.getId()));
-		refreshSuppliers();
-	}
-
-	public void refreshSuppliers() {
-		suppliers = supplierService.getAllSuppliers();
-	}
-
-	public void setSupplierService(SupplierService supplierService) {
-		this.supplierService = supplierService;
-	}
-
+	
 	public void setSupplier(Supplier supplier) {
 		this.supplier = supplier;
 	}
@@ -51,20 +25,12 @@ public class SupplierView extends BaseView implements Serializable {
 		return supplier;
 	}
 
-	public void newSupplier() {
+	@Override
+	public void newObject() {
+		super.newObject();
 		supplier = new Supplier();
-		setTitle("Новый " + getSign().toLowerCase());
 	}
 
-	public void saveSupplierAction() {
-		if (this.supplier.getId() != null) {
-			Supplier supplier = supplierService.getSupplier(this.supplier.getId());
-			supplier.copy(this.supplier);
-			this.supplier = supplier;
-		}
-		this.supplier = supplierService.saveSupplier(supplier);
-		refreshSuppliers();
-	}
 
 	@Override
 	public String getSign() {
@@ -79,11 +45,5 @@ public class SupplierView extends BaseView implements Serializable {
 	@Override
 	public IdentifiedEntity getBeanObject() {
 		return supplier;
-	}
-
-	@Override
-	public void refreshObjects() {
-		// TODO Auto-generated method stub
-		
 	}
 }
