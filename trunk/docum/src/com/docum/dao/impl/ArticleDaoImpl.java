69a4eb7eb07ba7e032eeb7865641de79a37be8e0
alekseyzet@gmail.com <alekseyzet@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-06-01 21:40:35 +0000

    Товары, категории, характеристики

--- trunk/docum/src/com/docum/dao/impl/ArticleDaoImpl.java
+++ trunk/docum/src/com/docum/dao/impl/ArticleDaoImpl.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Repository;
 
 import com.docum.dao.ArticleDao;
 import com.docum.domain.po.common.ArticleCategory;
+import com.docum.domain.po.common.ArticleFeature;
+import com.docum.domain.po.common.ArticleFeatureInstance;
 
 @Repository
 public class ArticleDaoImpl extends BaseDaoImpl implements ArticleDao {
@@ -19,6 +21,25 @@ public class ArticleDaoImpl extends BaseDaoImpl implements ArticleDao {
 		query.setParameter("articleId", articleId);
 		@SuppressWarnings("unchecked")
 		List<ArticleCategory> result = query.getResultList();
+		return result;
+	}
+
+	@Override
+	public List<ArticleFeature> getArticleFeatureByArticle(Long articleId) {
+		Query query = entityManager.createNamedQuery(GET_ARTICLE_FEATURES_BY_ARTICLE_QUERY);
+		query.setParameter("articleId", articleId);
+		@SuppressWarnings("unchecked")
+		List<ArticleFeature> result = query.getResultList();
+		return result;
+	}
+
+	@Override
+	public List<ArticleFeatureInstance> getArticleFeatureInstanceByArticle(Long articleFeatureId) {
+		Query query = 
+			entityManager.createNamedQuery(GET_ARTICLE_FEATURE_INSTANCE_BY_ARTICLE_QUERY);
+		query.setParameter("articleFeatureId", articleFeatureId);
+		@SuppressWarnings("unchecked")
+		List<ArticleFeatureInstance> result = query.getResultList();
 		return result;
 	}
 
