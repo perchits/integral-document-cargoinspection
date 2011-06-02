80f0d37d7bb2232c6b5d1463eb0b32f1e3f04876
alekseyzet@gmail.com <alekseyzet@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-06-02 13:42:30 +0000

    Косметика

--- docum/WebContent/resources/js/base.js
+++ docum/WebContent/resources/js/base.js
@@ -1,8 +1,10 @@
 function togglePanels(f) {
 	if (f) {
 		jQuery(".ui-icon-plusthick").click();
+		jQuery(".ui-icon-circle-triangle-e").click();
 	} else {
 		jQuery(".ui-icon-minusthick").click();
+		jQuery(".ui-icon-circle-triangle-e").click();
 	}
 }
 
