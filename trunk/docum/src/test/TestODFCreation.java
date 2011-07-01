152d536ee968d8034407725f555780e0c47ae6ae
alekseyzet@gmail.com <alekseyzet@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-07-01 08:21:38 +0000

    Продолжение эксперимента с созданием ODF файла

--- trunk/docum/src/test/TestODFCreation.java
+++ trunk/docum/src/test/TestODFCreation.java
@@ -1,8 +1,13 @@
 package test;
 
+import java.net.URI;
+
 import junit.framework.TestCase;
 
 import org.odftoolkit.odfdom.doc.OdfTextDocument;
+import org.odftoolkit.odfdom.dom.element.draw.DrawFrameElement;
+import org.odftoolkit.odfdom.dom.element.draw.DrawImageElement;
+import org.odftoolkit.odfdom.dom.element.table.TableTableCellElement;
 import org.odftoolkit.odfdom.incubator.doc.draw.OdfDrawImage;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -14,6 +19,9 @@ public class TestODFCreation extends TestCase {
 	public void testODFCreation() {
 		try {
 			OdfTextDocument odt = OdfTextDocument.loadDocument(LOCATION + "/testTemplate.odt");
+			odt.save(LOCATION + "/testResult.odt");
+			odt = OdfTextDocument.loadDocument(LOCATION + "/testResult.odt");
+			odt.getPackage().insert(new URI(LOCATION + "/1.png"), "Pictures/1.png", null);
 			int l = odt.getContentDom().getChildNodes().getLength();
 			for (int i = 0; i < l; i++) {
 				Node node = odt.getContentDom().getChildNodes().item(i);
@@ -29,6 +37,20 @@ public class TestODFCreation extends TestCase {
 	private void processNode(Node node) {
 		if (node.getNodeValue() != null) {
 			System.out.println(node.getNodeName() + " : " + node.getNodeValue());
+		}
+		if (node instanceof TableTableCellElement) {
+			TableTableCellElement tableCellElement = (TableTableCellElement) node;
+			DrawFrameElement drawFrameElement = tableCellElement.newDrawFrameElement();
+			drawFrameElement.setAttribute("draw:z-index", "0");
+			drawFrameElement.setAttribute("draw:style-name", "fr1");
+			drawFrameElement.setAttribute("svg:height", "6.283cm");
+			drawFrameElement.setAttribute("svg:width", "8.371cm");
+			DrawImageElement drawImageElement = drawFrameElement.newDrawImageElement();
+			drawImageElement.setXlinkHrefAttribute("Pictures/1.pngs");
+			drawImageElement.setXlinkActuateAttribute("onLoad");
+			drawImageElement.setXlinkShowAttribute("embed");
+			drawImageElement.setXlinkTypeAttribute("simple");
+			//System.out.println(node.getNodeName() + " : " + node.getNodeValue());
 		}
 		if (node instanceof OdfDrawImage) {
 			System.out.println(" " + node.getAttributes().getNamedItem("xlink:href").getNodeValue());
