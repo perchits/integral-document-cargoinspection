3e6c4e1a711ed45a47d7fc4bb7407937659221ed
3.14.casso.75@gmail.com <3.14.casso.75@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-07-11 16:32:44 +0000

    Диалог контейнеров. Рефакторинг

--- trunk/docum/src/com/docum/view/container/FeatureDlgView.java
+++ trunk/docum/src/com/docum/view/container/FeatureDlgView.java
@@ -5,45 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import com.docum.domain.po.common.Article;
-import com.docum.domain.po.common.ArticleFeature;
-import com.docum.domain.po.common.ArticleFeatureInstance;
 import com.docum.domain.po.common.CargoArticleFeature;
-import com.docum.service.ArticleService;
 import com.docum.view.AbstractDlgView;
 import com.docum.view.DialogActionEnum;
 
 public class FeatureDlgView extends AbstractDlgView implements Serializable {
 	private static final long serialVersionUID = -3474736648929694055L;
-	private ArticleService articleService;
+	
 	private Set<CargoArticleFeature> cargoFeatures;
-	private List<ArticleFeatureInstance> featureInstances;
-	private List<ArticleFeature> articleFeatures;
-
-	public List<ArticleFeature> getArticleFeatures() {
-		return articleFeatures;
-	}
-
-	public List<ArticleFeatureInstance> getFeatureInstances() {
-		return featureInstances;
-	}
-
+	
 	public void setCargoFeature(Set<CargoArticleFeature> features) {
 		this.cargoFeatures = features;
-	}
-
-	public List<ArticleFeatureInstance> getFeatureInstancesById(Long id){
-		return articleService.getArticleFeatureInstanceByArticle(id);
-	}
+	}	
 	
-	public FeatureDlgView(Set<CargoArticleFeature> features,
-			ArticleService articleService, Article article) {
-		this.articleService = articleService;
-		this.cargoFeatures = features;
-		featureInstances = articleService
-				.getArticleFeatureInstanceByArticle(article.getId());
-		articleFeatures = articleService.getArticleFeatureByArticle(article
-				.getId());
+	public FeatureDlgView(Set<CargoArticleFeature> features) {		
+		this.cargoFeatures = features;		
 	}
 
 	public List<CargoArticleFeature> getCargoFeatures() {
@@ -51,8 +27,8 @@ public class FeatureDlgView extends AbstractDlgView implements Serializable {
 	}
 
 	public String getTitle() {						
-		return "Редактирование свойств для груза " + 
-			getCargoFeatures().get(0).getCargo().toString();
+		return String.format("Редактирование свойств для груза «%1$s»" , 
+			getCargoFeatures().get(0).getCargo().toString());
 	}
 
 	public void save() {
