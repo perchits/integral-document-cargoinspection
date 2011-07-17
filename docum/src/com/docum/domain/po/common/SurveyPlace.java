287e41349e851e6bb83eaa707645b4654e27e9e2
andrewsha <andrewsha@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-07-17 13:03:03 +0000

    Class for survey place handbook

---
+++ docum/src/com/docum/domain/po/common/SurveyPlace.java
@@ -0,0 +1,29 @@
+package com.docum.domain.po.common;
+
+import javax.persistence.Entity;
+
+import com.docum.domain.po.IdentifiedEntity;
+import com.docum.util.EqualsHelper;
+import com.docum.util.HashCodeHelper;
+
+@Entity
+public class SurveyPlace extends IdentifiedEntity {
+
+	private static final long serialVersionUID = 3121763126213905142L;
+	
+	private String englishName;
+	private String russianName;
+	
+	public SurveyPlace() {
+	}
+
+	public SurveyPlace(SurveyPlace sp) {
+		copy(sp);
+	}
+	
+	
+
+	
+	
+	
+}
