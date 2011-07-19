f0729632fe29ae595289aec1dd1ab096ebd920d4
alekseyzet@gmail.com <alekseyzet@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-07-19 08:07:47 +0000

    Продолжение подготовки для ввода результата инспекции

---
+++ trunk/docum/src/com/docum/dao/InspectionBriefDao.java
@@ -0,0 +1,10 @@
+package com.docum.dao;
+
+import com.docum.domain.po.common.Container;
+import com.docum.domain.po.common.InspectionBrief;
+
+public interface InspectionBriefDao extends BaseDao{
+	public static final String GET_INSPECTION_BRIEF_BY_CONTAINER_QUERY = "getInspectionBriefByContainer";
+	
+	public InspectionBrief getInspectionBriefByContainer(Container container);
+}
