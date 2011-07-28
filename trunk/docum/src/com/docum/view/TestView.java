9a60eb6000b77da205c9a1a35c654a91086f97a8
perchits@gmail.com <perchits@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-07-28 19:38:18 +0000

    Сделал загрузку файлов с помощью tomagawk Пример здесь: http://localhost:8080/docum/test.jsf

--- trunk/docum/src/com/docum/view/TestView.java
+++ trunk/docum/src/com/docum/view/TestView.java
@@ -1,9 +1,14 @@
 package com.docum.view;
 
+import java.io.IOException;
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.myfaces.custom.fileupload.UploadedFile;
+import org.primefaces.event.FileUploadEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -12,11 +17,13 @@ import com.docum.domain.po.common.City;
 import com.docum.service.BaseService;
 
 @Controller("testView")
-@Scope("view")
+@Scope("session")
 public class TestView implements Serializable{
 	private static final long serialVersionUID = 5827007837538386824L;
 	@Autowired
 	BaseService baseService;
+
+	private UploadedFile uploadedFile;
 	
 	public Object save() {
 		City city = new City("Питер", "Piter", false);
@@ -34,4 +41,23 @@ public class TestView implements Serializable{
 	public City getCity() {
 		return baseService.getObject(City.class, 2L);
 	}
+	
+	public void handleFileUpload() throws IOException {  
+        String fileName = FilenameUtils.getName(uploadedFile.getName());
+        String contentType = uploadedFile.getContentType();
+        byte[] bytes = uploadedFile.getBytes();
+
+        // Now you can save bytes in DB (and also content type?)
+
+        FacesContext.getCurrentInstance().addMessage(null, 
+            new FacesMessage(String.format("File '%s' of type '%s' successfully uploaded!", fileName, contentType)));
+    }  	
+
+    public UploadedFile getUploadedFile() {
+        return uploadedFile;
+    }
+
+    public void setUploadedFile(UploadedFile uploadedFile) {
+        this.uploadedFile = uploadedFile;
+    }
 }
