e56ab479a1c5c2b511e2de15aae0222db89d303c
alekseyzet@gmail.com <alekseyzet@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-08-17 23:12:58 +0000

    Добавление обработки свойств при создании отчета

--- trunk/docum/src/com/docum/service/ReportingService.java
+++ trunk/docum/src/com/docum/service/ReportingService.java
@@ -2,7 +2,6 @@ package com.docum.service;
 
 import java.util.List;
 
-import com.docum.domain.po.common.Container;
 import com.docum.domain.po.common.Report;
 
 public interface ReportingService {
@@ -11,7 +10,7 @@ public interface ReportingService {
 	public static final String REPORT_FILENAME_PREFIX = "/resultReport_";
 	public static final String REPORT_TEMPLATE_FILENAME = "/documTemplate.odt";
 
-	public void createReport(Container container, Long reportId);
+	public void createReport(Report report) throws Exception;
 	public List<Report> getReportsByVoyage(Long voyageId);
 	public boolean checkStarOfficeConnection();
 }
