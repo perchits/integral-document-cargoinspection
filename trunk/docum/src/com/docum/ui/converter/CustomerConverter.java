e401c0c4a2d5b88374cf5bb967dd9002b41c53f2
alekseyzet@gmail.com <alekseyzet@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-08-26 23:08:24 +0000

    Рефакторинг CustomerConverter-а

--- trunk/docum/src/com/docum/ui/converter/CustomerConverter.java
+++ trunk/docum/src/com/docum/ui/converter/CustomerConverter.java
@@ -1,46 +1,13 @@
 package com.docum.ui.converter;
 
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
-import org.springframework.web.jsf.FacesContextUtils;
-
 import com.docum.domain.po.common.Customer;
-import com.docum.service.BaseService;
 
 @FacesConverter(value = "customerConverter")
-public class CustomerConverter implements Converter {
+public class CustomerConverter extends IdentifiedEntityAbstractConverter<Customer> {
+	public CustomerConverter() {
+		super(Customer.class);
+	}
 	
-	private BaseService getBaseService(FacesContext ctx) {
-		BaseService svc = (BaseService) FacesContextUtils
-			.getWebApplicationContext(ctx).getBean("baseService");
-		return svc;
-	}
-	public Object getAsObject(FacesContext context, UIComponent component,
-			String value) {
-		if (value == null || value.trim().isEmpty()) {
-			return null;
-		}
-		try {
-			return getBaseService(context).getObject(Customer.class, Long.parseLong(value));
-		} catch (NumberFormatException e) {
-			return null;
-		}
-	}
-
-	public String getAsString(FacesContext context, UIComponent component,
-			Object value) {
-		Customer customer;
-		if (value instanceof Customer) {
-			customer = (Customer) value;
-			if (customer.getId() == null)
-				throw new IllegalArgumentException(
-						"Cannot convert Customer object with null id.");
-			return customer.getId().toString();
-		} else {
-			return "";
-		}
-	}
 }
