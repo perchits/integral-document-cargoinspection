37ae6b99c7d2908cd8f999e2ffc1d337450822b9
3.14.casso.75@gmail.com <3.14.casso.75@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-08-26 21:40:55 +0000

    Добавил температуру и термоединицу в диалог инспекции и на форму контейнера поменял местами поля в диалоге инспекции

--- docum/src/com/docum/view/container/InspectionDlgView.java
+++ docum/src/com/docum/view/container/InspectionDlgView.java
@@ -3,6 +3,8 @@ package com.docum.view.container;
 import java.io.Serializable;
 import java.util.List;
 
+import com.docum.domain.po.common.ActualCargoCondition;
+import com.docum.domain.po.common.DeclaredCargoCondition;
 import com.docum.domain.po.common.Inspection;
 import com.docum.domain.po.common.NormativeDocument;
 import com.docum.domain.po.common.SurveyPlace;
@@ -14,12 +16,71 @@ import com.docum.view.DialogActionEnum;
 public class InspectionDlgView extends AbstractDlgView implements Serializable {
 	private static final long serialVersionUID = -6995707358505279412L;
 	private Inspection inspection;
+		
 	private List<SurveyPlace> surveyPlaces;
 	private List<Surveyor> surveyors;
 	private List<NormativeDocument> documents;
+	
+	public Double getMinTemperature() {		 
+		return getDeclaredCondition().getMinTemperature();
+	}
 
+	public void setMinTemperature(Double temperature) {		  
+			getDeclaredCondition().setMinTemperature(temperature);				
+	}
+	
+	public Double getMaxTemperature() {		 
+		return getDeclaredCondition().getMaxTemperature();
+	}
+
+	public void setMaxTemperature(Double temperature) {		  
+			getDeclaredCondition().setMaxTemperature(temperature);				
+	}	
+	
+	public Double getTemperature(){
+		return getActualCondition().getTemperature();
+	}
+	
+	public void setTemperature(Double temperature){
+		getActualCondition().setTemperature(temperature);
+	}
+	
+	public Boolean getHasTemperatureSpy(){
+		return getActualCondition().getHasTemperatureSpy();
+	}
+	
+	public void setHasTemperatureSpy(Boolean hasSpy){
+		getActualCondition().setHasTemperatureSpy(hasSpy);
+	}
+	
+	public Boolean getHasTemperatureTestDeviation(){ 
+		return getActualCondition().getHasTemperatureTestDeviation();
+	}
+	
+	public void setHasTemperatureTestDeviation(Boolean hasTemperatureTestDeviation) {
+		getActualCondition().setHasTemperatureTestDeviation(hasTemperatureTestDeviation);
+	}
+
+	public Boolean getHasTemperatureSpyDeviation() {
+		return getActualCondition().getHasTemperatureSpyDeviation();
+	}
+
+	public void setHasTemperatureSpyDeviation(Boolean hasTemperatureSpyDeviation) {
+		getActualCondition().setHasTemperatureSpyDeviation(hasTemperatureSpyDeviation);
+	}
+	
+	public DeclaredCargoCondition getDeclaredCondition(){
+		return inspection != null?
+				inspection.getContainer().getDeclaredCondition() : null;
+	}
+	
+	public ActualCargoCondition getActualCondition(){
+		return inspection != null?
+				inspection.getContainer().getActualCondition() : null;
+	}
+	
 	public InspectionDlgView(Inspection inspection, BaseService baseService) {
-		this.inspection = inspection;
+		this.inspection = inspection;		
 		surveyPlaces = baseService.getAll(SurveyPlace.class);
 		surveyors = baseService.getAll(Surveyor.class);
 		documents = baseService.getAll(NormativeDocument.class);
