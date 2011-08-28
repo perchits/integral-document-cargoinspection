e98e99fcc1653b708d731c7b4d652502806566e6
andrewsha <andrewsha@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-08-28 18:27:37 +0000

    Добавил справочник портов.

---
+++ docum/src/com/docum/view/dict/PortView.java
@@ -0,0 +1,46 @@
+package com.docum.view.dict;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.docum.domain.po.IdentifiedEntity;
+import com.docum.domain.po.common.Port;
+
+@Controller("portBean")
+@Scope("session")
+public class PortView extends BaseView {
+
+	private static final long serialVersionUID = 4140404602418969773L;
+	private static final String sign = "Порт";
+	
+	private Port port = new Port();
+	
+	public Port getPort() {
+		return port;
+	}
+	
+	public void setPort(Port port) {
+		this.port = port;
+	}
+	
+	@Override
+	public void newObject() {
+		super.newObject();
+		this.port = new Port();
+	}
+
+	@Override
+	public String getSign() {
+		return sign;
+	}
+
+	@Override
+	public String getBriefInfo() {
+		return port.getName();
+	}
+
+	@Override
+	public IdentifiedEntity getBeanObject() {
+		return port != null ? this.port : new Port();
+	}
+}
