e0e581ef1ccc09a8716ade9ef6cb8cb39020b078
perchits@gmail.com <perchits@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-09-16 18:48:32 +0000

    Сделал расчет партий груза по повреждениям. Пока не проверено.

--- trunk/docum/src/com/docum/service/StatsService.java
+++ trunk/docum/src/com/docum/service/StatsService.java
@@ -3,8 +3,10 @@ package com.docum.service;
 import java.util.List;
 
 import com.docum.domain.Stats;
+import com.docum.domain.po.common.Cargo;
 
 public interface StatsService {
 	public static final String SERVICE_NAME = "statsService";
 	public List<Stats.CargoParty> getCargoParties(Long invoiceId);
+	public Stats.CargoDefects extractCargoDefects(Cargo cargo);
 }
