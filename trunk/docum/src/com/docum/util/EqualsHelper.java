00338892870d6ba6e9e0d76969ad84b3e80c8c30
perchits@gmail.com <perchits@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-09-20 15:35:00 +0000

    Исправил баг со сравнением сущностей с пустыми id

--- trunk/docum/src/com/docum/util/EqualsHelper.java
+++ trunk/docum/src/com/docum/util/EqualsHelper.java
@@ -4,6 +4,9 @@ public class EqualsHelper
 {
     public static boolean equals(Object o1, Object o2)
     {
-        return o1 == null ? o2 != null : o1.equals(o2);
+        if(o1 == null || o2 == null)
+        	return false;
+        else
+        	return o1.equals(o2);
     }
 }
