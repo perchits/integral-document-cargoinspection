b0da78cec84df099b4055e8360633d7113e29978
3.14.casso.75@gmail.com <3.14.casso.75@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-10-10 11:12:59 +0000

    Обработка выдохшейся сессии

--- docum/src/com/docum/exception/DocumExceptionHandler.java
+++ docum/src/com/docum/exception/DocumExceptionHandler.java
@@ -4,6 +4,8 @@ import java.util.Iterator;
 
 import javax.faces.FacesException;
 import javax.faces.application.FacesMessage;
+import javax.faces.application.NavigationHandler;
+import javax.faces.application.ViewExpiredException;
 import javax.faces.context.ExceptionHandler;
 import javax.faces.context.ExceptionHandlerWrapper;
 import javax.faces.context.FacesContext;
@@ -27,21 +29,29 @@ public class DocumExceptionHandler extends ExceptionHandlerWrapper {
 
 	@Override
 	public void handle() throws FacesException {
-		for (Iterator<ExceptionQueuedEvent> i = getUnhandledExceptionQueuedEvents()
-				.iterator(); i.hasNext();) {
+		for (Iterator<ExceptionQueuedEvent> i = getUnhandledExceptionQueuedEvents().iterator(); i
+				.hasNext();) {
 			ExceptionQueuedEvent event = i.next();
-			ExceptionQueuedEventContext context = (ExceptionQueuedEventContext) event
-					.getSource();
+			ExceptionQueuedEventContext context = (ExceptionQueuedEventContext) event.getSource();
 			Throwable t = context.getException();
 			FacesContext fc = FacesContext.getCurrentInstance();
+
 			try {
-				if (t.getCause() != null) {
-					fc.addMessage(null, new FacesMessage(
-						FacesMessage.SEVERITY_ERROR, "Ошибочка вышла...", t.getCause().getMessage()));
+				if (t instanceof ViewExpiredException) {
+					ViewExpiredException vee = (ViewExpiredException) t;
+					NavigationHandler nav = fc.getApplication().getNavigationHandler();
+					fc.getExternalContext().getFlash().put("expiredViewId", vee.getViewId());
+
+					nav.handleNavigation(fc, null, "/login?faces-redirect=true");
+					fc.renderResponse();
 				}
-				fc.renderResponse();
+
+				if (t.getCause() != null) {
+					fc.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+							"Ошибочка вышла...", t.getCause().getMessage()));
+				}				
 				DocumLogger.log(t);
-				// redirect to error view etc....				
+				// redirect to error view etc....
 			} finally {
 				// after exception is handeled, remove it from queue
 				i.remove();
