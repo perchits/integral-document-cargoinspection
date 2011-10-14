41763e6a26cf5734a0fc02a1274ae9439e83f9eb
alekseyzet@gmail.com <alekseyzet@gmail.com@a36b0e6a-c020-cf75-0b65-dbdb6256e3c3>
2011-10-14 08:03:30 +0000

    Подготовка к сквозной нумерации отчетов внутри года

--- docum/src/com/docum/dao/ReportingDao.java
+++ docum/src/com/docum/dao/ReportingDao.java
@@ -6,7 +6,8 @@ import com.docum.domain.po.common.Report;
 
 public interface ReportingDao extends BaseDao {
 	public static final String GET_REPORTS_BY_VOYAGE_QUERY = "getReportsByVoyage";
+	public static final String GET_REPORT_NUMBER_WITHIN_YEAR = "getReportsWithinYear";
 	
 	public List<Report> getReportsByVoyage(Long voyageId);
-
+	public Long getReportsWithinYear(int year);
 }
